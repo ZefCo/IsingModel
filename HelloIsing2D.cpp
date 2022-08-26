226f2a70e4fe67aa125bc05c1d0119c327dcf295
ZefCo <zefram.cochrane2024@gmail.com>
2022-08-25 21:51:15 -0500

    Working on updating energy within class

--- HelloIsing2D.cpp
+++ HelloIsing2D.cpp
@@ -4,13 +4,13 @@
 
 
 
-int random_value() 
+int random_spin() 
 {
     std::random_device rd; // obtain a random number from hardware
     std::mt19937 gen(rd()); // use this number to create a  seed
-    std::uniform_int_distribution<> distr(0, 1); // define the range
+    std::uniform_int_distribution<> rpin(0, 1); // define the range
 
-    int num = distr(gen);
+    int num = rpin(gen);
 
     if (num == 0) {num = -1;}
 
@@ -19,45 +19,35 @@ int random_value()
 }
 
 
+int random_position(int p)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> rosition(0, p);
 
-// typedef struct LocalSpin
-// {
-//     int spin = 0;
-//     // double energy;
+    return rosition(gen);
 
-//     operator int();
-// };
-// LocalSpin::operator int()
-// {
-//     return spin;
-// }
+}
+
+
+
+struct LocalSpin
+{
+    int spin = random_spin();
+
+    operator int();
+};
+LocalSpin::operator int()
+{
+    return spin;
+}
 
 
 
 class IsingLattice
 {
 private:
-    // Place spins on the lattice
-    std::vector< std::vector<int> > create_lattice() {
-        std::vector< std::vector<int> > a;
 
-        a.resize(M);
-        for (int i = 0; i < M; i++) 
-        {
-            a[i].resize(N);
-        }
-
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                // std::cout << r;
-                a[i][j] = random_value();
-            }
-        }
-
-        return a;
-    }
-
-public:
     // Rows
     int M;
     // Columns
@@ -68,23 +58,110 @@ public:
     // External Mag Field
     double B;
 
-    std::vector<std::vector<int>> lattice;
+    // Place spins on the lattice
+    std::vector< std::vector<int> > create_lattice() {
+        std::vector< std::vector<int> > lattice;
 
-    double Energy() {
-        double e = 0;
+        // int lattice[M][N];
 
-        for (int i = 0; i < M; i++)
-        {
-            for (int j = 0; j < N; j++) 
-            {
-                e = e + (B*lattice[i][j]);
+
+        lattice.resize(M);
+        for (int i = 0; i < M; i++) {
+            lattice[i].resize(N);
+        }
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                // std::cout << r;
+                // lattice[i][j] = random_spin();
+                // random_spin() = lattice[i][j];
+                int v = random_spin();
+                lattice[i][j] = v;
+
             }
         }
+
+        std::cout << "M = " << M << " N = " << N << std::endl;
+
+        return lattice;
+    }
+
+public:
+
+    double energy;
+    std::vector<std::vector<int>> lattice;
+
+    double CalcEnergy() {
+        double e = 0.0;
+        double eb;
+        double eiu; // nearest neighbor up
+        double eid; // nearest neighbor down
+        double ejl; // nearest neighbor left
+        double ejr; // nearest neighbor right
+
+        int iu; // up one
+        int id; // down one
+        int jl; // left one
+        int jr; // right one
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+
+                // std::cout << "i: " << i << " j: " << j << std::endl;
+
+                eb = -B*lattice[i][j];
+
+                iu = i - 1;
+                if ((iu) < 0) {
+                    eiu = 0.0;
+                } else {
+                    eiu = -J * lattice[iu][j] * lattice[i][j];
+                    // std::cout << "\t" << iu << " - " << j <<  " - " << eiu << std::endl;
+                }
+
+                id = i + 1;
+                if ((id) >= M) {
+                    eid = 0.0;
+                } else {
+                    eid = -J * lattice[id][j] * lattice[i][j];
+                    // std::cout << "\t" << id << " - " << j <<  " - " << eid << std::endl;
+                }
+
+                jl = j - 1;
+                if ((jl) < 0) {
+                    ejl = 0.0;
+                } else {
+                    ejl = -J * lattice[i][jl] * lattice[i][j];
+                    // std::cout << "\t" << i << " - " << jl << " - " << ejl << std::endl;
+                }
+
+                jr = j + 1;
+                if ((jr) >= N) {
+                    ejr = 0.0;
+                } else {
+                    ejr = -J * lattice[i][jr] * lattice[i][j];
+                    // std::cout << "\t" << i << " - " << jr << " - " << ejr << std::endl;
+                }
+
+                e = e + eb + eiu + eid + ejl + ejr;
+            }
+        }
+
+        // std::cout << "E in Function " << e << std::endl;
         return e;
     }
 
+    void print_lattice() {
+        for (int i = 0; i < M; i++){
+            for (int j = 0; j < N; j++){
+                std::cout << lattice[i][j] << "\t";
+            }
+            std::cout << "\n";
+        }
+    }
+
     // Constructor
-    IsingLattice(int rows = 10, int columns = 10, double coupeling = 1.0, double magfield = 1.0)
+    IsingLattice(int rows, int columns, double coupeling, double magfield)
     {
         M = rows;
         N = columns;
@@ -99,31 +176,60 @@ public:
 
 main()
 {
-    int R;
-    int C;
+    const int R = 2; const int C = 2; const double B = 1.0; const double J = 1.0;
+
+    // int random_xy[2] = {0, 0}; // Empty array for storing xy position in later
+    int max_steps = 10;
     
-    std::cout << "Input Dimensions of RxC" << std::endl;
-    std::cout << "Rows: ";
-    std::cin >> R;
-    std::cout << "Columns: ";
-    std::cin >> C;
+    // std::cout << "Input Dimensions of RxC" << std::endl;
+    // std::cout << "Rows: ";
+    // std::cin >> R;
+    // std::cout << "Columns: ";
+    // std::cin >> C;
+
+    std::cout << "Max Steps: " << max_steps << std::endl;
     std::cout << "Dimensions: " << R << " x " << C << std::endl;
     
-    IsingLattice ising_lattice(R, C);
+    // std::cout << "Input External Field and Coupeling Constant" << std::endl;
+    // std::cout << "External Field B (float/double - default 1.0): ";
+    // std::cin >> B;
+    // std::cout << "Coupling Constant J (float/double - default 1.0): ";
+    // std::cin >> J;
 
-    std::cout << "J = " << ising_lattice.J << " B = " << ising_lattice.B << std::endl;
-    std::cout << "M = " << ising_lattice.M << " N = " << ising_lattice.N << std::endl;
+    std::cout << "External Field and Coupling Constant - B = " << B << " J = " << J <<std::endl;
 
-    for (int i = 0; i < R; i++) 
-    {
-        for (int j = 0; j < C; j++) 
-        {
-            std::cout << ising_lattice.lattice[i][j] << "\t" ;
+    IsingLattice ising_lattice(R, C, J, B);
+
+    std::cout << "Initial Lattice" << std::endl;
+    for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                std::cout << ising_lattice.lattice[i][j] << "\t" ;
+            }
+            std::cout << "\n";
         }
-        std::cout << "\n";
-    }
 
-    std::cout << "E = " << ising_lattice.Energy();
+    ising_lattice.energy = ising_lattice.CalcEnergy();
+
+    std::cout << "Initial Energy = " << ising_lattice.energy << std::endl;
+
+    for (int s = 0; s < max_steps; s++) {
+        int r = random_position(R - 1); int c = random_position(C - 1);
+    
+        // std::cout << "S: " << s <<" R = " << r << " C = " << c << std::endl;
+        ising_lattice.lattice[r][c] = ising_lattice.lattice[r][c] * -1;
+
+        // ising_lattice.print_lattice();
+
+        std::cout << "Updated Energy = " << ising_lattice.energy << std::endl;
+        }
+
+
+    //     // std::cout << ising_lattice.lattice[x][y];
+
+    // }
+
+
+    // std::cout << "E = " << ising_lattice.Energy();
 
 }
 
