d3192413185c98f4943d534c69a4926aa5c7e0d4
ZefCo <zefram.cochrane2024@gmail.com>
2022-09-14 19:11:50 -0500

    Start working on homework or fix E

--- SpeakmanIsing2D.cpp
+++ SpeakmanIsing2D.cpp
@@ -6,87 +6,92 @@
 # include <fstream>
 # include <numeric>
 # include <string>
+# include "numRec.h"
 
 // Declaring seed to be a global variable. Trying to avoid declaring anything else as global
-long seed;
+int seed;
 
 
+/* RAN2 from Numerical Methods in C
+Keeping this here in case I want to use it. It works, but it works differently then ran2 from cpp. This chunk came from Numerical Recipies in C
+as given by Dr. Bassler. The ran2 used here is from Numerical Recipies in C++, which is written by the same people but as you can see uses some values differently.
+**/
 // definitions for ran2
-# define IM1 2147483563
-# define IM2 2147483399
-# define AM (1.0/IM1)
-# define IMM1 (IM1-1)
-# define IA1 40014
-# define IA2 40692
-# define IQ1 53668
-# define IQ2 52774
-# define IR1 12211
-# define IR2 3791
-# define NTAB 32
-# define NDIV (1+IMM1/NTAB)
-# define EPS 1.2e-7
-# define RNMX (1.0-EPS)
+// # define IM1 2147483563
+// # define IM2 2147483399
+// # define AM (1.0/IM1)
+// # define IMM1 (IM1-1)
+// # define IA1 40014
+// # define IA2 40692
+// # define IQ1 53668
+// # define IQ2 52774
+// # define IR1 12211
+// # define IR2 3791
+// # define NTAB 32
+// # define NDIV (1+IMM1/NTAB)
+// # define EPS 1.2e-7
+// # define RNMX (1.0-EPS)
 
-// ran2, copied from page 282 of Numerical Recipies (defines at top)
-float ran2(long *idum)
-{
+// // ran2, copied from page 282 of Numerical Recipies (defines at top)
+// float ran2(long *idum)
+// {
 
-    int j;
-    long k;
-    static long idum2 = 123456789;
-    static long iy = 0;
-    static long iv[NTAB];
-    float temp;
+//     int j;
+//     long k;
+//     static long idum2 = 123456789;
+//     static long iy = 0;
+//     static long iv[NTAB];
+//     float temp;
 
-    if (*idum <= 0) {                     // This just initilzes
-        if (-(*idum) < 1) *idum = 1;  // prevents idum from being 0
-        else *idum = -(*idum);
+//     if (*idum <= 0) {                     // This just initilzes
+//         if (-(*idum) < 1) *idum = 1;  // prevents idum from being 0
+//         else *idum = -(*idum);
 
-        idum2 = (*idum);
+//         idum2 = (*idum);
 
-        for (j = NTAB + 7; j >=0; j--) {  // shuffles the values
-            k = (*idum) / IQ1;
-            *idum = IA1 * (*idum - k*IQ1) - k*IR1;  // pretty sure this is *idum - (k*IQ1), hence the lack of spacing
-            if (*idum < 0) *idum += IM1; 
-            if (j < NTAB) iv[j] = *idum;
-        }
-        iy = iv[0];
-    }
+//         for (j = NTAB + 7; j >=0; j--) {  // shuffles the values
+//             k = (*idum) / IQ1;
+//             *idum = IA1 * (*idum - k*IQ1) - k*IR1;  // pretty sure this is *idum - (k*IQ1), hence the lack of spacing
+//             if (*idum < 0) *idum += IM1; 
+//             if (j < NTAB) iv[j] = *idum;
+//         }
+//         iy = iv[0];
+//     }
 
-    k = (*idum) / IQ1;
-    *idum = IA1 * (*idum - k*IQ1) - k*IQ1;
+//     k = (*idum) / IQ1;
+//     *idum = IA1 * (*idum - k*IQ1) - k*IQ1;
 
-    if (*idum < 0) *idum += IM1;
+//     if (*idum < 0) *idum += IM1;
 
-    k = idum2 / IQ2;
-    idum2 = IA2 * (idum2 - k*IQ2) - k*IQ2;
+//     k = idum2 / IQ2;
+//     idum2 = IA2 * (idum2 - k*IQ2) - k*IQ2;
 
-    if (idum2 < 0) idum2 += IM2;
+//     if (idum2 < 0) idum2 += IM2;
 
-    j = iy / NDIV;
-    iy = iv[j] - idum2;
-    iv[j] = *idum;
+//     j = iy / NDIV;
+//     iy = iv[j] - idum2;
+//     iv[j] = *idum;
 
-    if (iy < 1) iy += IMM1;
-    if ((temp = AM* iy) > RNMX) return RNMX;
-    else return temp;
+//     if (iy < 1) iy += IMM1;
+//     if ((temp = AM* iy) > RNMX) return RNMX;
+//     else return temp;
 
-}
+// }
 
-#undef IM1
-#undef IM2
-#undef AM
-#undef IMM1
-#undef IA1
-#undef IA2
-#undef IQ1
-#undef IQ2
-#undef IR1
-#undef IR2
-#undef NTAB
-#undef NDIV
-#undef EPS
-#undef RNMX
+// #undef IM1
+// #undef IM2
+// #undef AM
+// #undef IMM1
+// #undef IA1
+// #undef IA2
+// #undef IQ1
+// #undef IQ2
+// #undef IR1
+// #undef IR2
+// #undef NTAB
+// #undef NDIV
+// #undef EPS
+// #undef RNMX
 
 
 
@@ -95,8 +100,9 @@ float ran2(long *idum)
 int random_int(int min = 0, int max = 2)
 {
 
-    float rval = ran2(&seed);
+    float rval = NR::ran2(seed);
     int i = min + ((max - min) * rval);
+    // std::cout << "Rval: " << rval << " Rnt: " << i << std::endl;
 
     return i;
 }
@@ -109,6 +115,7 @@ int random_spin()
     int spin;
     spin = random_int();
     if (spin == 0) { spin = -1; }
+    // std::cout << "Spin: " << spin << std::endl;
 
     return spin;
 
@@ -134,7 +141,7 @@ int flip_spin(float delta_e)
 {
     int flip = 1;
 
-    float roll = ran2(&seed);
+    float roll = NR::ran2(seed);
     if (roll < delta_e) {flip = -1;}
 
     return flip;
@@ -215,6 +222,22 @@ private:
     }
 
 
+    // Calculates the initial energy
+    int init_energy() {
+        int local_energy = 0;
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+
+                local_energy += DeltaEnergy(i, j);
+            }
+        }
+
+        return local_energy;
+
+    }
+
+
     // Calculates the initial magnitization
     int init_magnitization() {
 
@@ -254,7 +277,8 @@ private:
 
 public:
 
-    float energy; float mag; float Mag;
+    float energy, Energy; 
+    float mag, Mag;
     std::vector<std::vector<int>> lattice;
 
 
@@ -290,12 +314,14 @@ public:
 
 
     // You "flip" the spin outside the lattice, then pass it in to let the lattice figure out if it needs to be flipped
-    void flip_spin(int flip, int row, int col) {
+    void flip_spin(int flip, int row, int col, int e_mu) {
 
         if (flip == -1) {
             lattice[row][col] = lattice[row][col] * flip;
             Mag = Mag + (2 * lattice[row][col]);
             mag = Mag / get_total_spins();
+            Energy = Energy + e_mu;
+            energy = Energy / get_total_spins();
         }
 
     }
@@ -346,6 +372,8 @@ public:
 
         Mag = init_magnitization();
         mag = Mag / (get_total_spins());
+        Energy = init_energy();
+        energy = Energy / (get_total_spins());
 
     };
 
@@ -354,13 +382,14 @@ public:
 
 
 // writes the Mag and mag to a csv file
-void write_mags_csv(std::string filename, std::vector<float> Mags, std::vector<float> mags) {
+void write_csv(std::string filename, std::vector<float> Mags, std::vector<float> mags, std::vector<float> Energy, std::vector<float> energy) {
     std::ofstream fileout(filename);
 
-    fileout << "sweep,M,m\n";
+    fileout << "quarter,M,m,E,e\n";
 
-    for (int i = 0; i < Mags.size(); i++) {
-        fileout << (i + 1) << "," << Mags.at(i) << "," << mags.at(i) << "\n";
+    for (float i = 0; i < Mags.size(); i++) {
+        float index = ((i + 1) / 4);
+        fileout << index << "," << Mags.at(i) << "," << mags.at(i) << "," << Energy.at(i) << "," << energy.at(i) << "\n";
     }
 
     fileout.close();
@@ -372,14 +401,18 @@ void write_mags_csv(std::string filename, std::vector<float> Mags, std::vector<f
 main()
 {
 
-    float H; const float J = 1.0;
-    float T; int D; int max_sweeps; int print_mags_to_console; int print_first_lattice;
-    int flip; float r; int de_index; int he_index; float delta_e; float h_e;
-    std::vector<float> ave_mag_storage; std::vector<float> ave_Mag_storage;
+    const float J = 1.0; 
+    float T, H, r, delta_e, h_e; 
+    int D, max_sweeps, print_first_lattice, print_final_lattice;
+    int flip, de_index, he_index;
+    int init_seed;
+    // std::vector<float> ave_mag_storage; std::vector<float> ave_Mag_storage;
+    std::vector<float> mag_storage, Mag_storage, energy_storage, Energy_storage;
 
     std::cout << "Input Seed: ";
     std::cin >> seed;
-    
+    init_seed = seed;
+
     std::cout << "Input Dimensions of the lattice: RxC" << std::endl;
     std::cout << "Square Lattice Dimensions (M x M): ";
     std::cin >> D;
@@ -391,36 +424,41 @@ main()
     std::cout << "Input External Magnetic Field: ";
     std::cin >> H;
 
-    std::cout << "Input the Maximum Number of SWEEPS - \nSweep for this lattice would be " << D*D  << " Steps: ";
+    std::cout << "Input the Maximum Number of SWEEPS\nSweep for this lattice would be " << D*D  << " Steps\nSweeps: ";
     std::cin >> max_sweeps;
 
-    std::cout << "\nDo you want to see the Mag and mag output at the end?\nA high Max Steps may clutter the console\n1 for yes, 0 for no: ";
-    std::cin >> print_mags_to_console;
 
     std::cout << "\nInputs:" << std::endl;
     std::cout << "Seed: " << seed << std::endl;
     std::cout << "Dimensions of Lattice: " << D << " x " << D << std::endl;
     std::cout << "Tempurature: " << T << "\tB Field: " << H << "\tJ = k (1, dimensionless)" << std::endl;
     std::cout << "Sweeps: " << max_sweeps << "\tTotal Steps: " << max_sweeps * sweep_steps << std::endl;
+    std::cout << "Measurement will be taken every QUARTER sweep" << std::endl;
 
     // pause_for_input();
 
     IsingLattice ising_lattice(D, D, T, J, H);
 
-    // ising_lattice.print_boltzman();
 
     std::cout << "Total Spins: " << ising_lattice.get_total_spins() << std::endl;
     std::cout << "Initial Mag: " << ising_lattice.Mag << "\tInitial Mag per Spin: " << ising_lattice.mag << std::endl;
+    std::cout << "Initial Energy: " << ising_lattice.Energy << std::endl;
+    // ising_lattice.print_boltzman();
 
-    
-
-    std::cout << "Do you wish to see the inital lattice? 1 for yes 0 for no: ";
+    std::cout << "\nDo you wish to see the inital lattice? 1 for yes 0 for no: ";
     std::cin >> print_first_lattice;
 
     if (print_first_lattice == 1) {
             std::cout << "Initial Lattice" << std::endl;
             ising_lattice.print_lattice();
     }
+
+    // exit(0);
+
+    // std::cout << "\nDo you want to see the Mag and mag output at the end?\nA high Max Steps may clutter the console\n1 for yes, 0 for no: ";
+    // std::cin >> print_mags_to_console;
+    std::cout << "\nDo you wish to see the final Lattice at the end?\n1 for yes, 0 for no: ";
+    std::cin >> print_final_lattice;
 
     std::cout << "Ready to begin simulation" << std::endl;
     pause_for_input();
@@ -429,59 +467,78 @@ main()
     // it's still working.
     for (int s = 1; s < (max_sweeps + 1); s++) {
         std::vector<float> swe_mag_storage; std::vector<float> swe_Mag_storage;
-        for (int j = 0; j < sweep_steps; j++){
-            r = random_int(0, D); int c = random_int(0, D);
+        // Taking the measurement every 1/4 sweep. Doing a for loop in a for loop makes the most sense
+        for (int q = 1; q <=4; q++) {
+            for (int j = 0; j < (sweep_steps / 4); j++){
+                r = random_int(0, D); int c = random_int(0, D);
 
-            de_index = ising_lattice.DeltaEnergy(r, c);  // These two actually do *not* return the energy, but instead return the key to the values of their energy
-            he_index = ising_lattice.lattice[r][c];  // You have to use these indicies to get the proper energy
+                de_index = ising_lattice.DeltaEnergy(r, c);  // These two actually do *not* return the energy, but instead return the key to the values of their energy
+                he_index = ising_lattice.lattice[r][c];  // You have to use these indicies to get the proper energy
 
-            delta_e = ising_lattice.get_boltzman(de_index, he_index);
+                delta_e = ising_lattice.get_boltzman(de_index, he_index);
 
-            int flip = flip_spin(delta_e);
+                int flip = flip_spin(delta_e);
 
-            ising_lattice.flip_spin(flip, r, c);
+                ising_lattice.flip_spin(flip, r, c, de_index + he_index);
 
-            // // These commented out sections are for when I want to see it step by step. Useful for when this started, but probably not
-            // // going to come back into effect anytime soon.
-            // if (flip == -1) {
-            //     ising_lattice.lattice[r][c] = ising_lattice.lattice[r][c] * flip;
-            //     ising_lattice.Mag = ising_lattice.Mag + (2 * ising_lattice.lattice[r][c]);
-            //     ising_lattice.mag = ising_lattice.Mag / ising_lattice.get_total_spins();
-            //     // std::cout << "--- Flipping Spin ---" << std::endl;
-            // }
+                // std::cout << "Energy = " << ising_lattice.Energy << " e = " << ising_lattice.energy << std::endl;
+                // exit(0);
 
-            swe_mag_storage.push_back(ising_lattice.mag); // store each mag for averaging later
-            swe_Mag_storage.push_back(ising_lattice.Mag); // store each Mag for averaging later
+                // // These commented out sections are for when I want to see it step by step. Useful for when this started, but probably not
+                // // going to come back into effect anytime soon.
+                // if (flip == -1) {
+                //     ising_lattice.lattice[r][c] = ising_lattice.lattice[r][c] * flip;
+                //     ising_lattice.Mag = ising_lattice.Mag + (2 * ising_lattice.lattice[r][c]);
+                //     ising_lattice.mag = ising_lattice.Mag / ising_lattice.get_total_spins();
+                //     // std::cout << "--- Flipping Spin ---" << std::endl;
+                // }
 
-            // std::cout << "S: " << s <<" R = " << r << " C = " << c << std::endl;
-            // std::cout << "J-E (Index) = " << de_index << "\tH-E (Index) = " << he_index << "\tDelta-E = " << delta_e <<  std::endl;
-            // // std::cout << "Flip is " << flip << std::endl;
-            // std::cout << "Mag = " << ising_lattice.Mag << "\tMag per Spin = " << ising_lattice.mag << std::endl;
+                // the changes to Mag and mag were already taken care of in ising_lattice.flip_spin(), so I can just pull the values now
+                // swe_mag_storage.push_back(ising_lattice.mag); // store each mag for averaging later
+                // swe_Mag_storage.push_back(ising_lattice.Mag); // store each Mag for averaging later
 
-            // std::cout << "Updated Lattice: " << std::endl;
-            // ising_lattice.print_lattice();
+                // std::cout << "S: " << s <<" R = " << r << " C = " << c << std::endl;
+                // std::cout << "J-E (Index) = " << de_index << "\tH-E (Index) = " << he_index << "\tDelta-E = " << delta_e <<  std::endl;
+                // // std::cout << "Flip is " << flip << std::endl;
+                // std::cout << "Mag = " << ising_lattice.Mag << "\tMag per Spin = " << ising_lattice.mag << std::endl;
 
-            // pause_for_input();
+                // std::cout << "Updated Lattice: " << std::endl;
+                // ising_lattice.print_lattice();
+
+                // pause_for_input();
+            }
+        
+            // Grabs the last mag and Mag of the sweep and pushes it into the vector. mag and Mag were found at ising_lattice.flip_spin()
+            // So I can just grab the last value
+            mag_storage.push_back(ising_lattice.mag);
+            Mag_storage.push_back(ising_lattice.Mag);
+            energy_storage.push_back(ising_lattice.energy);
+            Energy_storage.push_back(ising_lattice.Energy);
+
+            // // In case you want to see when the steps are completed. Useful for knowing it's still working.
+            // std::cout << "Completed Sweep " << s << std::endl;
+            // std::cout << "Random Seed to this point " << seed << std::endl;
         }
-
-        ave_mag_storage.push_back(ave_vector(swe_mag_storage));
-        ave_Mag_storage.push_back(ave_vector(swe_Mag_storage));
-        // // In case you want to see when the steps are completed. Useful for knowing it's still working.
-        // std::cout << "Completed Sweep " << s << std::endl;
+        // exit(0);
 
     }
 
-    if (print_mags_to_console == 1) {    
-        std::cout << "Magnitization\tMagnitization per spin" << std::endl;
-        for (int i = 0; i < ave_Mag_storage.size(); i++) {
-            std::cout << ave_Mag_storage[i] << "\t\t" << ave_mag_storage[i] << std::endl;
-        }
+    // if (print_mags_to_console == 1) {    
+    //     std::cout << "Magnitization\tMagnitization per spin" << std::endl;
+    //     for (int i = 0; i < Mag_storage.size(); i++) {
+    //         std::cout << Mag_storage[i] << "\t\t" << mag_storage[i] << std::endl;
+    //     }
+    // }
+
+    if (print_final_lattice == 1) {
+        std::cout << "Final Lattice:" << std::endl;
+        ising_lattice.print_lattice();
     }
 
-    std::string filename = "magMag_Temp_" + std::to_string(T) + "_Size_" + std::to_string(D) + "x" + std::to_string(D) + "_seed_" + std::to_string(seed) + ".csv";
+    std::string filename = "mME_Temp_" + std::to_string(T) + "_Size_" + std::to_string(D) + "x" + std::to_string(D) + "_seed_" + std::to_string(init_seed) + "_sweeps_" + std::to_string(max_sweeps) + ".csv";
 
-    write_mags_csv(filename, ave_Mag_storage, ave_mag_storage);
-    std::cout << "Mag and mag were output to the file " << filename << std::endl;
+    write_csv(filename, Mag_storage, mag_storage, Energy_storage, energy_storage);
+    std::cout << "Mag, mag, E, and e were output to the file " << filename << std::endl;
 
 
 }
--- plotM.py
+++ plotM.py
@@ -6,14 +6,14 @@ import pathlib
 import pandas
 import re
 
-# with open(pathlib.Path.cwd() / "magMag_Temp_5.000000_Size_20x20_seed_639699332.csv") as infile:
+# with open(pathlib.Path.cwd() / "DataFiles" / "mME_Temp_2.000000_Size_100x100_seed_123456789_sweeps_50000.csv") as infile:
 #     data = pandas.read_csv(infile, header=0)
 
 # # # print(data)
 
-# # fig = px.scatter(data, x = "sweep", y = "m", title = "")
+# fig = px.scatter(data, x = "sweep", y = "m", title = "")
 # # plotly.io.write_image(fig, "magT2L100x100.pdf", format="pdf")
-# # # fig.show()
+# fig.show()
 
 # his = px.histogram(data, x = "m")
 # # plotly.io.write_image(his, "Hist_magT2L100x100.pdf", format="pdf")
@@ -25,8 +25,10 @@ import re
 # Combine Files
 csv_folder = pathlib.Path.cwd() / "DataFiles"
 
-# temps = []
+# # temps = []
 monte_files = pandas.DataFrame()
+eonte_files = pandas.DataFrame()
+# scat_files = pandas.DataFrame()
 for thing in csv_folder.iterdir():
     # print(type(thing))
     if thing.is_file() and thing.suffix in ".csv":
@@ -34,43 +36,73 @@ for thing in csv_folder.iterdir():
         thingy = re.split("_", str(thing))
         # temp = re.sub(r"\.", ",", thingy[2])
         temp = re.sub(r"000$", "", thingy[2])
-        temp = f"{temp} K"
+        temp = f"{temp}K"
         # print(temp)
+        histbins = thingy[4]
+
+        title = f"{temp}_{str(thingy[6])}"
 
         with open(thing) as thing_file:
             thing_data = pandas.read_csv(thing_file, header = 0)
 
-        m_data = thing_data["m"].to_list()
-
-        data_series = pandas.Series(m_data, name = temp)
+        m_data = pandas.Series(thing_data["m"].to_list(), name = title, index = thing_data["quarter"])
+        e_data = pandas.Series(thing_data["E"].to_list(), name = title, index = thing_data["quarter"])
         # print(data_series)
 
-        monte_files = pandas.concat([monte_files, data_series.to_frame().T])
+        monte_files = pandas.concat([monte_files, m_data.to_frame().T])
+        eonte_files = pandas.concat([eonte_files, e_data.to_frame().T])
+
 
 monte_files = monte_files.T
+# monte_files = monte_files / monte_files.sum(axis=0)
+# monte_files.index = list(thing_data.index)
+eonte_files = eonte_files.T
+
+
+# Everything should be the same size that gets dumped into the folder, so I can take the last of the histbins, which
+# is actually the dimensions of the lattice. Split that along x, since it's LxL, then square L (after changing it to an
+# int), and that's how many bins I'll need.
+histbins = re.split("x", histbins)[0]
+histbins = int(histbins) * int(histbins)
+
 
 
 # legends = []
+sca = go.Figure()
 his = go.Figure()
 for title in list(monte_files.columns):
-    his.add_trace(go.Histogram(x = monte_files[title], histnorm = 'probability density', name = title, nbinsx=500))
+    his.add_trace(go.Histogram(x = monte_files[title], name = title, nbinsx=histbins))
+    sca.add_trace(go.Scatter(x = list(monte_files.index), y = monte_files[title], name = title))
     # legends.append(title)
 
+# print(monte_files[title])
 
-# for 
-# his.add_trace(go.Histogram(monte_files))
-his.update_layout(barmode="overlay", title = "<m> at different temps")
-# his.update_l
+his.update_layout(barmode="overlay", title = "Distribution of m - quarter sweeps")
 his.update_traces(opacity=0.75)
 his.update_yaxes(showticklabels=False, visible=False)
-his.update_xaxes(title_text = "<m>")
-his.show()
-plotly.io.write_image(his, "Hist_mag_20x20.pdf", format="pdf")
+his.update_xaxes(title_text = "m")
+
+
+sca.update_layout(title = "m per sweep")
+sca.update_xaxes(title_text = "[Quarter] Sweep")
+sca.update_yaxes(title_text = "m")
+
+# his.show()
+# plotly.io.write_image(his, "Hist_mag_20x20.pdf", format="pdf")
 
 # print(monte_files)
 
-# his = px.histogram(monte_files, nbins=250)
-# his.show()
+his.show()
+sca.show()
 
-        # third one is the 
-# print(temp)
+
+# legends = []
+sca = go.Figure()
+for title in list(eonte_files.columns):
+    sca.add_trace(go.Scatter(x = list(eonte_files.index), y = eonte_files[title], name = title))
+
+sca.update_layout(title = "E per sweep")
+sca.update_xaxes(title_text = "[Quarter] Sweep")
+sca.update_yaxes(title_text = "E")
+
+sca.show()
